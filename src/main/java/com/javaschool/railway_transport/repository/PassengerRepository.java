cc7ad2fef0125cff574127331ca733b3ba54bbc3
José Manuel Molina <jose.manuel.molina2001@gmail.com>
2023-03-13 19:21:51 +0100

    Repositories

--- src/main/java/com/javaschool/railway_transport/repository/PassengerRepository.java
+++ src/main/java/com/javaschool/railway_transport/repository/PassengerRepository.java
@@ -4,6 +4,8 @@ import com.javaschool.railway_transport.model.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PassengerRepository extends JpaRepository<Passenger,Long> {
 
--- src/main/java/com/javaschool/railway_transport/repository/TrainRepository.java
+++ src/main/java/com/javaschool/railway_transport/repository/TrainRepository.java
@@ -4,6 +4,7 @@ import com.javaschool.railway_transport.model.Train;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface TrainRepository extends JpaRepository<Train, Long>{
